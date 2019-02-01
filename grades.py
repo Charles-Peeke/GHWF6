cb3fa18c60684b59d50b167bd828b813e13309b8
Charles-Peeke <stcdp03@moravian.edu>
2019-02-01 13:57:31 -0500

    Test and Implementation for dropping a number of grades

--- grades.py
+++ grades.py
@@ -1,5 +1,5 @@
 
-def compute_hw_average(grades):
-    if len(grades) == 0:
+def compute_hw_average(grades, num_dropped):
+    if len(grades) == 0 or num_dropped >= len(grades):
         return 0
     return sum(grades)/len(grades)
--- test_grades.py
+++ test_grades.py
@@ -4,14 +4,19 @@ from grades import compute_hw_average
 
 def test_zero_grades():
     grades = []
-    assert compute_hw_average(grades) == 0
+    assert compute_hw_average(grades, 0) == 0
 
 
 def test_single_grade():
     grades = [42]
-    assert compute_hw_average(grades) == 42
+    assert compute_hw_average(grades, 0) == 42
 
 
 def test_two_grades():
     grades = [2.0,3.0]
-    assert compute_hw_average(grades) == 2.5
+    assert compute_hw_average(grades, 0) == 2.5
+
+
+def test_one_grade_one_dropped():
+    grades = [12]
+    assert compute_hw_average(grades, 1) == 0
